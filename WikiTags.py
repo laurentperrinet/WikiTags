55ce33b60d6522e651c5218e102bdc0ee2b6f41d
Laurent Perrinet <laurent.perrinet@gmail.com>
2020-03-17 15:18:42 +0100

    Update WikiTags.py
    make a list of new bands

--- WikiTags.py
+++ WikiTags.py
@@ -10,17 +10,34 @@ BANDS = ['Talking Heads',
          'Bob Dylan',
          'Georges Brassens',
          'Banana Rama',
+         'Bob Marley',
+         'Peter Tosh',
          'The Clash',
          'Mulatu Astatke',
          'Joe Strummer',
          'Rolling Stones',
          'Stone Roses',
          ]
+VERB = True
+N_X, N_Y = 8, 15 # columns, rows
 
 # generating a bunch of new names
+## extarcting both sides
 bands_pre, bands_post = [], []
 for band in BANDS:
     (s1, s2) = band.split(' ')
     bands_pre.append(s1)
     bands_post.append(s2)
-print(bands_pre, bands_post)
+if VERB: print(bands_pre, bands_post)
+
+## shuffling them
+import itertools
+bands_list = list(itertools.product(bands_pre, bands_post))
+import numpy as np
+np.random.seed(SEED)
+ind_bands = np.random.permutation(len(BANDS)**2)
+if VERB:
+    for i_band in ind_bands:
+        print(bands_list[i_band])
+
+#import os
