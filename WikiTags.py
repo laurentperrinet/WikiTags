b5d74f22910da07492cd3c51f44cb92fb8f3adcd
Laurent Perrinet <laurent.perrinet@gmail.com>
2020-03-17 15:57:19 +0100

    first grid
    generating grid

--- WikiTags.py
+++ WikiTags.py
@@ -56,7 +56,7 @@ import pyqrcode
 for i_band in ind_bands:
     s1, s2 = bands_list[i_band]
     figname = os.path.join('/tmp', s1 + s2 + '.png')
-    if not os.isfile(figname):
+    if not os.path.isfile(figname):
         print('Generating', figname)
         url = ROOT + s1 + s2
         code = pyqrcode.create(url)
@@ -69,3 +69,28 @@ for i_band in ind_bands:
 # >>> code = pyqrcode.create('Are you suggesting coconuts migrate?')
 # >>> image_as_str = code.png_as_base64_str(scale=5)
 # >>> html_img = '<img src="data:image/png;base64,{}">'.format(image_as_str)
+
+import matplotlib.pyplot as plt
+
+# Config:
+dpi = 100 # dots per inches
+figsize = 400/dpi # in inches
+
+# Create plt plot:
+fig, axs = plt.subplots(N_Y, N_X,
+                        figsize=(N_X*figsize, N_Y*figsize))
+
+print(len(axs), len(axs[0]))
+for ii, i_band in enumerate(ind_bands[:(N_X*N_Y)]):
+    s1, s2 = bands_list[i_band]
+    figname = os.path.join('/tmp', s1 + s2 + '.png')
+
+    x_position = ii % N_X
+    y_position = ii // N_X
+    print(ii, '/ (', x_position, ',', y_position, ') : ', figname)
+
+    plt_image = plt.imread(figname)
+    axs[y_position, x_position].imshow(plt_image, cmap=plt.gray())
+
+plt.subplots_adjust(left=0.0, right=1.0, bottom=0.0, top=1.0)
+plt.savefig(RESULT, dpi=dpi)
