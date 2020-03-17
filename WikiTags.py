38b185069fc3ee16e09eae429fb1f8b4b21c2ffa
Laurent Perrinet <laurent.perrinet@gmail.com>
2020-03-17 15:48:31 +0100

    Update WikiTags.py
    generating the individual QR codes

--- WikiTags.py
+++ WikiTags.py
@@ -20,9 +20,10 @@ BANDS = ['Talking Heads',
          ]
 VERB = True
 N_X, N_Y = 8, 15 # columns, rows
+RESULT = './grid.jpg'
 
 # generating a bunch of new names
-## extarcting both sides
+## extracting both sides
 bands_pre, bands_post = [], []
 for band in BANDS:
     (s1, s2) = band.split(' ')
@@ -38,6 +39,33 @@ np.random.seed(SEED)
 ind_bands = np.random.permutation(len(BANDS)**2)
 if VERB:
     for i_band in ind_bands:
-        print(bands_list[i_band])
+        s1, s2 = bands_list[i_band]
+        print(s1 + s2)
 
-#import os
+
+if VERB:
+    url = ROOT + s1 + s2
+    print(url)
+
+import os
+import pyqrcode
+# https://pythonhosted.org/PyQRCode/rendering.html
+# pip3 install pyqrcode
+# pip3 install pypng
+
+for i_band in ind_bands:
+    s1, s2 = bands_list[i_band]
+    figname = os.path.join('/tmp', s1 + s2 + '.png')
+    if not os.isfile(figname):
+        print('Generating', figname)
+        url = ROOT + s1 + s2
+        code = pyqrcode.create(url)
+        code.png(figname, scale=5)
+
+# alternatives
+# image_as_str = code.png_as_base64_str(scale=5)
+# html_img = '<img src="data:image/png;base64,{}">'.format(image_as_str)
+# print(code.text())
+# >>> code = pyqrcode.create('Are you suggesting coconuts migrate?')
+# >>> image_as_str = code.png_as_base64_str(scale=5)
+# >>> html_img = '<img src="data:image/png;base64,{}">'.format(image_as_str)
